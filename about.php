0a9ecc339d5af6090a13187da5d775fa67d72a51
PU-MEriksson <malin.eriksson83@icloud.com>
2024-10-08 14:04:29 +0200

    Added html and some text

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About me</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<h1>About me</h1>
+<p>Hi! <br>
+My name is Malin, and I’m a former social worker transitioning to become a web developer. 
+This is one of my first attempts at creating a webpage.</p>
+</body>
+</html>
