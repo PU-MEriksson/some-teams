b3b6a30b51360866a3097fcdbd17064e210c5567
PU-MEriksson <malin.eriksson83@icloud.com>
2024-10-08 11:27:55 +0200

    Added some html and a foreach loop to the index

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Some Teams</title>
+</head>
+
+
+<body>
+
+<?php
+require __DIR__.'/data.php';
+?>
+
+<main>
+
+    <?php
+    foreach ($teams as $teamName => $teamData): ?>
+        <article>
+            <img src="<?=$teamData['logo'];?>"alt="<?=$teamName;?>">
+            <h2><?=$teamName?></h2>
+            <p>
+                League: <?=$teamData['league'];?> <br>
+                City: <?=$teamData['city']?> <br>
+                Rank: <?=$teamData['uefa-coefficient-ranking']?> <br>
+                Group: <?=$teamData['group']?> <br>
+                <a href="<?=$teamData['url']?>">Official webpage</a>
+            </p>
+            
+        </article>
+    <?php endforeach; ?>
+
+</main>
+
+
+
+</body>
+</html>
